4950e4fa4ca4e98e2ebb986d8ed84074192fbc5d
Guillaume Poisson <guillaumepoisson91@gmail.com>
2023-10-31 01:07:58 +0100

    feat(benchmark): Added kNodes/s for big perft

--- tests/tools/run_perft.py
+++ tests/tools/run_perft.py
@@ -2,6 +2,7 @@ import argparse
 import os
 import os.path
 import subprocess
+import time
 
 os.system('color')
 
@@ -9,23 +10,35 @@ good = 0
 fail = 0
 errors = []
 
+min_nodes_bench = 5000000
+speed_sum = 0
+nb_benchmarked = 0
+
 no_color = '\033[0m'
 good_color = '\033[90m'
 fail_color = '\033[91m'
 
-def print_test_result(test_name, ref_result, engine_result):
+def print_test_result(test_name, ref_result, engine_result, duration):
     name = test_name.name.split('/')[-1]
 
-    if ref_result == int(engine_result):
-        print(f"{good_color}[ PASSED ]{no_color} ", end="")
-        print(f"{name}: {ref_result}")
+    if ref_result == int(engine_result) or ref_result == -1:
+        print(f"{good_color}[ PASSED ]{no_color} ", f"{name}: {engine_result}", end = "")
+
+        if (engine_result >= min_nodes_bench):
+            speed = int(engine_result / duration / 1000)
+            global speed_sum, nb_benchmarked
+            speed_sum += speed
+            nb_benchmarked += 1
+            print(f" | Speed: {speed} kNodes/s", end="")
+
+        print(flush=True)
 
         global good
         good += 1
 
     else:
         err = f"{fail_color}[ FAILED ]{no_color} {name}: Got {engine_result} but {ref_result} was expected"
-        print(err)
+        print(err, flush=True)
         errors.append(err)
 
         global fail
@@ -33,6 +46,7 @@ def print_test_result(test_name, ref_result, engine_result):
 
 
 def perft_test_file(perft_file_path, engine_path):
+    start = time.time()
     try:
         engine_perft = int(
             subprocess.run([engine_path, '--perft', perft_file_path],
@@ -40,6 +54,7 @@ def perft_test_file(perft_file_path, engine_path):
                            encoding="utf-8").stdout)
     except Exception as e:
         raise Exception("Could not read the result of your engine. Make sure it is a valid unsigned integer printed on stdout.")
+    end = time.time()
 
     with open(perft_file_path) as p:
         perft_line = str.strip(p.readline())
@@ -50,8 +65,9 @@ def perft_test_file(perft_file_path, engine_path):
         if os.path.isfile(file):
             with open(file, "r") as f:
                 ref_perft = int(f.readline())
-                print_test_result(p, ref_perft, engine_perft)
-
+                print_test_result(p, ref_perft, engine_perft, end - start)
+        else:
+            print_test_result(p, -1, engine_perft, end - start)
 
 def run_perft_directory(perft_dir_path, engine_path, large):
     for dir in sorted([d for d in os.listdir(perft_dir_path)]):
@@ -92,5 +108,9 @@ if __name__ == "__main__":
 
     large = args.large.lower() == "true"
     res = run_perft_directory(args.perft_directory, args.binary, large)
+
+    if (nb_benchmarked > 0):
+        print(f"Average speed: {int(speed_sum / nb_benchmarked)} kNodes/s")
+
     if not res:
         exit(1)
